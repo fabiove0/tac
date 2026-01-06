1b0d16f254142578b816db298f11d4841a50a95c
fabiove0 <146615902+fabiove0@users.noreply.github.com>
2026-01-06 14:06:10 -0400

    Update app.py

--- app.py
+++ app.py
@@ -157,6 +157,28 @@ else:
         .tabela-relatorio thead tr:first-child {
             display: none;
         }
+            td:contains("CONCLUÍDO") {
+            background-color: #d4edda;
+            color: #155724;
+            font-weight: bold;
+        }
+        
+        td:contains("EM ANDAMENTO") {
+            background-color: #fff3cd;
+            color: #856404;
+            font-weight: bold;
+        }
+        
+        td:contains("NÃO INICIADO") {
+            background-color: #f8d7da;
+            color: #721c24;
+            font-weight: bold;
+        }
+        
+        td:contains("NÃO SE APLICA") {
+            background-color: #e2e3e5;
+            color: #383d41;
+        }
     </style>
     """, unsafe_allow_html=True)
 
