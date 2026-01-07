2c70193f961bc7938f23f195e24e5a006fc09f32
fabiove0 <146615902+fabiove0@users.noreply.github.com>
2026-01-07 10:41:15 -0400

    Update app.py

--- app.py
+++ app.py
@@ -109,6 +109,10 @@ lista_status = ['Todos'] + sorted(df_tratado['STATUS_DA_CLAUSULA'].unique().toli
 
 st.sidebar.image("logo_sejus.png", use_container_width=True)
 st.sidebar.header("Filtros")
+st.sidebar.header("NGER- Núcleo de Gestão Estratégica de Resultados")
+st.sidebar.subheader("Equipe (Amanda L. B. Silva, Ana C. P. Silva, Bernardo M. Filho, Daiane B. Fernandes, Débora F. Pimenta e Nicole Garcia) \n Desenvolvedor: Fabio V. Lima"
+
+
 
 escolha_tac = st.sidebar.selectbox("Selecione o Documento:", lista_tacs)
 escolha_status = st.sidebar.selectbox("Selecione o Status:", lista_status)
