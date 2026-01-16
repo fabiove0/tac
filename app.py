61f99bad428404f2628992a8131ae2ea735e1b6a
fabiove0 <146615902+fabiove0@users.noreply.github.com>
2026-01-16 09:10:27 -0400

    Update app.py

--- app.py
+++ app.py
@@ -484,7 +484,7 @@ else:
         ax_barra.set_xlabel("")             # remove título eixo X
         ax_barra.set_ylabel("")             # remove título eixo Y
         ax_barra.set_yticks([])
-        ax_barra.tick_params(axis='x', rotation=45, labelsize=7)
+        ax_barra.tick_params(axis='x', rotation=45, labelsize=3)
 
         ax_barra.legend(fontsize=3)
 
